47ab7a3cd9b92357b1c2533f872c3cc707a7aaf4
Aknaf Saputra <saputraaknaf301@gmail.com>
2025-09-04 21:00:14 +0700

    if-else, el-if statement

---
+++ el-if statement/main.py
@@ -0,0 +1,14 @@
+# elif = else if statement
+
+nama = input('nama kamu siapa?')
+
+if nama=='ucup':
+    print("halo ucup")
+elif nama == 'otong':
+    print("halo otong")
+elif nama == 'aknaf':
+    print('kamu ganteng')
+else:
+    print("aku tidak kenal kamu")
+
+print('akhir program')
---
+++ if-else statement/main.py
@@ -0,0 +1,27 @@
+# if dan else statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama anda?")
+print(nama)
+
+# if kondisi: aksi
+# 1. program if inline
+if nama=='ucup' : print("kamu sangat ganteng")
+print('akhir program')
+
+# 2. program if identation
+if nama =='ucup':
+    print('kamu sangat ganteng bro')
+    
+print(f"terima kasih {nama}")
+
+# 3. else statement
+if nama=='otong':
+    print('hai otong')
+else:
+    print('kamu bukan otong, gak keren')
+
+print('akhir program');
