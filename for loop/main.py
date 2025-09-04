25a46c636aabfbb5144a945a492cd783e93c7191
Aknaf Saputra <saputraaknaf301@gmail.com>
2025-09-04 21:35:59 +0700

    for-loop, while-loop

---
+++ for loop/main.py
@@ -0,0 +1,33 @@
+# perulangan (loop)
+
+
+# for kondisi:
+#   aksi
+
+# ini dengan list
+angka1 = [0,2,4,8,10]
+print(angka1)
+
+for i in angka1:
+    print(f"i sekarang -> {i}")
+    
+print('akhir program')
+
+# ini dengan range
+for i in range(5):
+    print(f"i sekarang -> {i}")
+    
+print('akhir program')
+
+for i in range(1,7):
+    print(f"i sekarang -> {i}")
+    
+print('akhir program')
+
+# menggunakan string
+data_str = "saya ganteng"
+
+for huruf in data_str:
+    print(huruf)
+    
+print("akhir program")
---
+++ while loop/main.py
@@ -0,0 +1,14 @@
+# while loop
+
+# while kondisi:
+#   aksi ini
+#   aksi itu
+# akhir program
+
+angka = 0
+
+while angka < 5:
+    angka += 1
+    print("aknaf kece")
+
+print("akhir program")
