ccb520631b2228d74009ceef0bd07a8b5c9005e8
Aknaf Saputra <saputraaknaf301@gmail.com>
2025-09-06 07:21:45 +0700

    tuples, set, dictionary

---
+++ dictionary/main.py
@@ -0,0 +1,18 @@
+# list -> array, mengakses data menggunakan index
+data_list = ['ucup','aknaf']
+print(data_list[1])
+
+# dictionary (dict) -> associative array
+# indentifier -> key
+data_dict = {
+    'key': 'value',
+    'cp': 'ucup',
+    'anp': 'aknaf',
+    'ddg': 'dudung',
+    'number': 100,
+    'list': data_list
+}
+print(data_dict)
+print(data_dict['cp'])
+print(data_dict['number'])
+print(data_dict['list'])
---
+++ tuples dan set/main.py
@@ -0,0 +1,17 @@
+# list
+data_list = [1,2,3,4,5]
+print(data_list)
+
+# tuples
+data_tuples = (5,4,3,2,1)
+print(data_tuples)
+print(data_tuples[2])
+
+# data_tuples[1] = "aknaf" (tidak bisa dilakukan)
+# data_tuples.append(1) (tidak bisa dilakukan)
+
+# sets
+data_sets = {10,9,3,5,2,7}
+print(data_sets)
+# print(data_sets[1]) (tidak bisa dilakukan)
+
